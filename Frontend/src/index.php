b1a8414ec3acf32423d33e5f5125e517f1b613ee
Vinura de Silva <m.v.r.desilva@gmail.com>
2023-12-09 08:03:06 +0530

    index file

---
+++ Frontend/src/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Product Management</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+        
+        .container {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #eab6ff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .section {
+            margin-bottom: 20px;
+        }
+
+
+        p {
+            color: #666;
+        }
+
+        a {
+            display: inline-block;
+            padding: 10px 20px;
+            margin-top: 10px;
+            text-decoration: none;
+            color: #fff;
+            background-color: #007bff;
+            border-radius: 4px;
+            transition: background-color 0.3s ease;
+            align-content: center;
+        }
+
+        a:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="section">
+            <div>
+                <div class="section">
+                    <p>You can manage products by Adding, Updating, Deleting products.</p>
+                    <a href="addProduct.php" >View All Current Products</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
